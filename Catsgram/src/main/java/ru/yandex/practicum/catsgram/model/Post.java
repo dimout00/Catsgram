f62e05639835c54953977911d7c43c3deae06271
Dimout00 <dimout00@yandex.ru>
2025-10-16 19:16:00 +0500

    Catsgram

---
+++ Catsgram/src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.catsgram.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import java.time.Instant;
+
+@Data
+@EqualsAndHashCode(of = "id")
+public class Post {
+    private Long id;
+    private Long authorId;
+    private String description;
+    private Instant postDate = Instant.now();
+}
